e7ce19c4ed530b0a3f1c3a46a119fcaf34b23dd7
MAYSMOHAIDAT <mohedat92@yahoo.com>
2024-02-29 11:11:36 -0500

    first commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,23 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin= {"pretty", 
+				"html:reports/cucumber_results.html",
+				"json:reports/cucumber_results.json"
+				},
+		features="./src/test/resources/Features",
+		glue="steps_definitions",
+		tags="@update_items"
+		)
+
+
+
+public class TestRunner {
+
+}
